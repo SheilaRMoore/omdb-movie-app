a93d46691fa061fd000ce8d3d06c64be67704bd6
Sheila R Moore <sherexmykes@github.com>
2020-12-20 21:35:31 -0500

    added search state and api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import MovieList from './components/MovieList';
 const  App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+	const getMovieRequest = async (searchValue) => {
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=bf3d3de4`;
+
+		const response = await fetch(url);
+		const responseJson = await response.json();
+
+		if (responseJson.Search) {
+			setMovies(responseJson.Search)
    return  <div>hello!</div>;
- 
+    }
 };
+
+useEffect(() => {
+  getMovieRequest(searchValue);
+}, [searchValue]);
+
 
 export default App;
